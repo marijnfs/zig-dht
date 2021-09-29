0857365833491693ba3cd7370d95446965f42ebd
Marijn Stollenga <stollenga@imfusion.de>
2021-09-30 00:39:12 +0200

    add some live typing

--- src/c.zig
+++ src/c.zig
@@ -14,6 +14,7 @@ var input_thread: std.Thread = undefined;
 
 var nc_context: ?*c.notcurses = undefined;
 var nc_plane: ?*c.ncplane = undefined;
+var nc_line_plane: ?*c.ncplane = undefined;
 
 pub fn print(str: []u8) void {
     for (str) |char| {
@@ -51,8 +52,19 @@ pub fn read_loop() !void {
             try jobs.enqueue(.{ .broadcast = message });
 
             try buf.resize(0);
+
+            c.ncplane_erase(nc_line_plane);
+            c.ncplane_home(nc_line_plane);
+            _ = c.notcurses_render(nc_context);
         } else {
             try buf.append(ecg);
+
+            var cell: c.nccell = undefined;
+            c.nccell_init(&cell);
+            _ = c.nccell_load_egc32(nc_line_plane, &cell, ecg);
+            _ = c.ncplane_putc(nc_line_plane, &cell);
+
+            _ = c.notcurses_render(nc_context);
         }
     }
 }
@@ -61,6 +73,14 @@ pub fn init() !void {
     nc_context = c.notcurses_init(null, c.stdout);
     nc_plane = c.notcurses_top(nc_context);
     _ = c.ncplane_set_scrolling(nc_plane, true);
+
+    var plane_options = std.mem.zeroes(c.ncplane_options);
+    plane_options.y = c.NCALIGN_BOTTOM;
+    plane_options.x = c.NCALIGN_LEFT;
+    plane_options.rows = 1;
+    plane_options.cols = 20;
+    plane_options.flags = c.NCPLANE_OPTION_HORALIGNED | c.NCPLANE_OPTION_VERALIGNED | c.NCPLANE_OPTION_FIXED;
+    nc_line_plane = c.ncplane_create(nc_plane, &plane_options);
 
     input_thread = try std.Thread.spawn(.{}, read_loop, .{});
 }
