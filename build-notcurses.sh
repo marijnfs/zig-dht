6179897a7e8bf115a5ccdbe59ff1eb81359e1e04
Marijn Stollenga <m.stollenga@gmail.com>
2021-11-18 22:49:48 +0100

    add install command

--- build-notcurses.sh
+++ build-notcurses.sh
@@ -4,4 +4,5 @@ mkdir -p ext/notcurses/build
 pushd ext/notcurses/build
 cmake -DUSE_DOCTEST:BOOL="0" -DUSE_CPP:BOOL="0" -DUSE_PANDOC:BOOL="0" -DUSE_POC:BOOL="0" -DUSE_MULTIMEDIA:STRING="none" ..
 make -j
+sudo make install
 popd
