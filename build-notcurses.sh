281d9bb333c1cb52118db0544e8cc7459b291e91
Marijn Stollenga <m.stollenga@gmail.com>
2021-11-18 22:50:41 +0100

    notcurses build script

--- build-notcurses.sh
+++ build-notcurses.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git submodule update --init
 mkdir -p ext/notcurses/build
 pushd ext/notcurses/build
 cmake -DUSE_DOCTEST:BOOL="0" -DUSE_CPP:BOOL="0" -DUSE_PANDOC:BOOL="0" -DUSE_POC:BOOL="0" -DUSE_MULTIMEDIA:STRING="none" ..
