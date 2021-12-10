5a4997c3a10fb50d6a533da433a0d4516ec23046
Marijn Stollenga <stollenga@imfusion.de>
2021-12-10 23:30:17 +0100

    no filetest

--- src/main.zig
+++ src/main.zig
@@ -69,7 +69,6 @@ fn filetest() !void {
 }
 
 pub fn main() !void {
-    try filetest();
     var args = try std.process.argsAlloc(default.allocator);
     defer std.process.argsFree(default.allocator, args);
     std.log.info("arg0 {s}", .{args[0]});
