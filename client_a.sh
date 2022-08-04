ae30e569cacdc2376a406a6e2bcfaebd3c6a020a
Marijn Stollenga <m.stollenga@gmail.com>
2022-08-04 23:56:51 +0200

    add test scripts

---
+++ client_a.sh
@@ -0,0 +1,3 @@
+#/bin/env sh
+./zig-out/bin/sync --ip 0.0.0.0 --port 8091 --remote_ip 0.0.0.0 --remote_port 8090
+
---
+++ client_b.sh
@@ -0,0 +1,2 @@
+#/bin/env sh
+./zig-out/bin/sync --ip 0.0.0.0 --port 8092 --remote_ip 0.0.0.0 --remote_port 8090
